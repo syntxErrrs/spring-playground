fa6169d8b2470dc18de385c1c8204ba4ee94400f
Jonathon Phillips <jonathan.phillips@austincc.edu>
2021-09-20 15:56:09 -0500

    cleaned up test

--- src/test/java/com/galvanize/springplayground/FlightControllerTest.java
+++ src/test/java/com/galvanize/springplayground/FlightControllerTest.java
@@ -17,22 +17,20 @@ class FlightControllerTest {
     @Autowired
     MockMvc mvc;
 
+    String depart = "2017-04-21 14:34";
+    String firstName = "Some name";
+    String lastName = "Some other name";
+    int price = 200;
+
     @Test
     void getFlightReturnsFlightInJsonFormat() throws Exception {
-        String firstName = "Some name";
-        String lastName = "Some other name";
-        int price = 200;
-        Passenger passenger = new Passenger(firstName, lastName);
-        Ticket ticket = new Ticket(passenger, price);
-        Ticket[] tickets = {ticket};
-
         this.mvc.perform(
                         get("/flights/flight")
                                 .accept(MediaType.APPLICATION_JSON)
                                 .contentType(MediaType.APPLICATION_JSON)
                 )
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.Departs", is("2017-04-21 14:34")))
+                .andExpect(jsonPath("$.Departs", is(depart)))
                 .andExpect(jsonPath("$.Tickets[0].Passenger.FirstName", is(firstName)))
                 .andExpect(jsonPath("$.Tickets[0].Passenger.LastName", is(lastName)))
                 .andExpect(jsonPath("$.Tickets[0].Price", is(price)));
@@ -40,21 +38,17 @@ class FlightControllerTest {
 
     @Test
     void getFlightsReturnsListOfFlightsInJsonFormat() throws Exception {
-        String firstName = "Some name";
-        String lastName = "Some other name";
-        int price = 200;
-
         this.mvc.perform(
                         get("/flights")
                                 .accept(MediaType.APPLICATION_JSON)
                                 .contentType(MediaType.APPLICATION_JSON)
                 )
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.[0].Departs", is("2017-04-21 14:34")))
+                .andExpect(jsonPath("$.[0].Departs", is(depart)))
                 .andExpect(jsonPath("$.[0].Tickets[0].Passenger.FirstName", is(firstName)))
                 .andExpect(jsonPath("$.[0].Tickets[0].Passenger.LastName", is(lastName)))
                 .andExpect(jsonPath("$.[0].Tickets[0].Price", is(price)))
-                .andExpect(jsonPath("$.[1].Departs", is("2017-04-21 14:34")))
+                .andExpect(jsonPath("$.[1].Departs", is(depart)))
                 .andExpect(jsonPath("$.[1].Tickets[0].Passenger.FirstName", is(lastName)))
                 .andExpect(jsonPath("$.[1].Tickets[0].Price", is(price + 200)));
     }
