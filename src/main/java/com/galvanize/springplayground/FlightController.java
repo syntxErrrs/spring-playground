37a4fc2d96f95788ba3d56c15a4b4854f0d79a20
Jonathon Phillips <jonathan.phillips@austincc.edu>
2021-09-21 13:01:06 -0500

    removed unusued import

--- src/main/java/com/galvanize/springplayground/FlightController.java
+++ src/main/java/com/galvanize/springplayground/FlightController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
